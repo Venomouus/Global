c380b45efaa6935aae09439b2df4e7146cd77217
Gustavo Nascimento <80040515+Venomouus@users.noreply.github.com>
2022-05-30 11:13:40 -0300

    Add files via upload
    Imagem renderizando 100% agora

--- Global/src/main/java/br/com/fiap/bean/VisitanteBean.java
+++ Global/src/main/java/br/com/fiap/bean/VisitanteBean.java
@@ -16,161 +16,101 @@ import org.primefaces.model.file.UploadedFile;
 import br.com.fiap.dao.VisitanteDao;
 import br.com.fiap.model.Visitante;
 
+
 @Named
 @RequestScoped
 public class VisitanteBean {
 
 	private Visitante visitante = new Visitante();
+	private List<Visitante> list;
+	private UploadedFile image;
 	
 	private VisitanteDao visitanteDao = new VisitanteDao();
 	
-	public void save1()  {
-
-		visitanteDao.create(getVisitante());
+	public VisitanteBean() {
+		list = this.list();
+	}
+	
+	public String save() throws IOException {
+		System.out.println(this.visitante);
+		
+		System.out.println(image.getFileName()); 
+		
+		ServletContext servletContext = (ServletContext) FacesContext
+															.getCurrentInstance()
+															.getExternalContext()
+															.getContext();
+		String servletPath = servletContext.getRealPath("/");
+		
+		System.err.println(servletPath);
+		
+		FileOutputStream out = 
+				new FileOutputStream(servletPath + "\\images\\" + image.getFileName());
+		out.write(image.getContent());
+		out.close();
+		
+		visitante.setImagePath("\\images\\" + image.getFileName());
+		
+		visitanteDao.create(visitante);
 		
 		FacesContext
 			.getCurrentInstance()
-			.addMessage(null, new FacesMessage("Usuário cadastrado com sucesso"));
+			.addMessage(null, new FacesMessage("Visitante cadastrado com sucesso"));
 		
+		return "Visitantes";
 	}
-	
-	public List<Visitante> getList() {
+		
+	public List<Visitante> list() {
 		return visitanteDao.listAll();
 	}
 	
-	public String cadastro(){
-		if (visitanteDao.exist(visitante)) {
-			//salvar o usuario logado na secao
-			FacesContext
-				.getCurrentInstance()
-				.getExternalContext()
-				.getSessionMap()
-				.put("nome", visitante);
-			
-			return "setups";
-		}
+	public String delete(Visitante visitante) {
+		visitanteDao.remove(visitante);
 		
 		FacesContext
 			.getCurrentInstance()
-			.getExternalContext()
+			.getExternalContext() 
 			.getFlash()
 			.setKeepMessages(true);
 		
 		FacesContext
 			.getCurrentInstance()
-			.addMessage(null, new FacesMessage("Login inválido"));
+			.addMessage(null, new FacesMessage("Visitante apagado com sucesso"));
+		
+		return "visitante?faces-redirect=true";
 
-		return "login?faces-redirect=true";
 	}
 	
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		
-	
-	@Named
-	@RequestScoped
-
-		private Visitante visitante1 = new Visitante();
-		private List<Visitante> list;
-		private UploadedFile image;
+	public void edit() {
+		visitanteDao.update(visitante);
 		
-		private VisitanteDao visitanteDao1 = new VisitanteDao();
-		
-		public VisitanteBean() {
-			list = this.list();
-		}
-		
-		public String save() throws IOException {
-			System.out.println(this.visitante);
-			
-			System.out.println(image.getFileName()); 
-			
-			ServletContext servletContext = (ServletContext) FacesContext
-																.getCurrentInstance()
-																.getExternalContext()
-																.getContext();
-			String servletPath = servletContext.getRealPath("/");
-			
-			System.err.println(servletPath);
-			
-			FileOutputStream out = 
-					new FileOutputStream(servletPath + "\\images\\" + image.getFileName());
-			out.write(image.getContent());
-			out.close();
-			
-			visitante.setImagePath("\\images\\" + image.getFileName());
-			
-			visitanteDao.create(visitante);
-			
-			FacesContext
-				.getCurrentInstance()
-				.addMessage(null, new FacesMessage("Visitante cadastrado com sucesso"));
-			
-			return "setups";
-		}
-			
-		public List<Visitante> list() {
-			return visitanteDao.listAll();
-		}
-		
-		public String delete(Visitante visitante) {
-			visitanteDao.remove(visitante);
-			
-			FacesContext
-				.getCurrentInstance()
-				.getExternalContext() 
-				.getFlash()
-				.setKeepMessages(true);
-			
-			FacesContext
-				.getCurrentInstance()
-				.addMessage(null, new FacesMessage("Visitante apagado com sucesso"));
-			
-			return "setups?faces-redirect=true";
+		FacesContext
+			.getCurrentInstance()
+			.addMessage(null, new FacesMessage("Visitante atualizado com sucesso"));
+	}
 
-		}
-		
-		public void edit() {
-			visitanteDao.update(visitante);
-			
-			FacesContext
-				.getCurrentInstance()
-				.addMessage(null, new FacesMessage("Setup atualizado com sucesso"));
-		}
+	public List<Visitante> getList() {
+		return list;
+	}
 
-		public List<Visitante> getList1() {
-			return list;
-		}
+	public void setList(List<Visitante> list) {
+		this.list = list;
+	}
 
-		public void setList(List<Visitante> list) {
-			this.list = list;
-		}
+	public Visitante getVisitante() {
+		return visitante;
+	}
 
-		public Visitante getVisitante() {
-			return visitante;
-		}
+	public void setVisitante(Visitante visitante) {
+		this.visitante = visitante;
+	}
 
-		public void setVisitante(Visitante visitante) {
-			this.visitante = visitante;
-		}
+	public UploadedFile getImage() {
+		return image;
+	}
 
-		public UploadedFile getImage() {
-			return image;
-		}
-
-		public void setImage(UploadedFile image) {
-			this.image = image;
-		}
-
-		public VisitanteDao getVisitanteDao1() {
-			return visitanteDao1;
-		}
-
-		public void setVisitanteDao1(VisitanteDao visitanteDao1) {
-			this.visitanteDao1 = visitanteDao1;
-		}
-
-
+	public void setImage(UploadedFile image) {
+		this.image = image;
+	}
 
 }
